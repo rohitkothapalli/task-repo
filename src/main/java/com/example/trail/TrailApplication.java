e855f924fdb8bb6f20689d8b128bb0a2b1bcb9ae
rohitkothapalli <60282687+rohitkothapalli@users.noreply.github.com>
2023-03-11 14:49:36 +0530

    Update TrailApplication.java

--- src/main/java/com/example/trail/TrailApplication.java
+++ src/main/java/com/example/trail/TrailApplication.java
@@ -15,7 +15,7 @@ public class TrailApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World! with a version 1.0.1";
     }
 }
 
